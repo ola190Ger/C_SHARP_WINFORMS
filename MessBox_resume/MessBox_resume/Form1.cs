f583d1367b632b47cc0c3e1a8f087af841418c5b
ola190Ger <49449139+ola190Ger@users.noreply.github.com>
2019-10-31 22:40:09 +0200

    Homework1_ShortRezume

---
+++ MessBox_resume/MessBox_resume/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessBox_resume
+{
+    public partial class Form1 : Form
+    {
+        int countSimbol = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result= MessageBox.Show("Name:\n\tOlga", "Rezume", MessageBoxButtons.OK);
+            countSimbol = "Name:\n\tOlga".Length;
+            if (result==DialogResult.OK)
+            {
+                countSimbol += "Education:\n\tChernivtsi National University, Department of Physics".Length;
+                result = MessageBox.Show("Education:\n\tChernivtsi National University, Department of Physics", "Rezume", MessageBoxButtons.OK);
+                if (result == DialogResult.OK)
+                {
+                    countSimbol += "Status:\n\tHave work now".Length;
+                    result = MessageBox.Show("Status:\n\tHave work now", $"Average: {countSimbol.ToString()}/3 = {countSimbol / 3} simb", MessageBoxButtons.OK);
+                    if (result == DialogResult.OK)
+                        Application.Exit();
+                }
+
+            }
+
+        }
+    }
+}
