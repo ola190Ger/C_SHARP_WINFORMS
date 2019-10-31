c87ca150f0004756e3ef6b4158f3725488593bac
ola190Ger <49449139+ola190Ger@users.noreply.github.com>
2019-10-31 23:51:33 +0200

    Homework1_3_Позициямыши

---
+++ Rectangle_dz2/Rectangle/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace RectangleIn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(237, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CLICK HERE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 373);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseClickk);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Rectangle_dz2/Rectangle/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RectangleIn
+{
+    public partial class Form1 : Form
+    {
+        RectangleF rect;
+        Point point_click = Point.Empty;
+        bool CTRL=false;
+        public Form1()
+        {
+            InitializeComponent();
+            rect = new RectangleF(10, 10, ClientSize.Width-10, ClientSize.Height-10);
+        }
+
+        private void MouseClickk(object sender, MouseEventArgs e)
+        {
+            if(e.Button==MouseButtons.Left)
+            {
+                point_click.X = e.X;
+                point_click.Y = e.Y;
+
+                if(rect.Contains(point_click))
+                {
+                    //Text ="("+ point_click.X+";"+point_click.Y+")"+" in rectangle";
+                    label1.Text = point_click.ToString() + " IN rectangle";
+                }
+                else
+                {
+                    label1.Text = point_click.ToString() + " OUTSIDE rectangle";
+
+                }
+            }
+            if (e.Button == MouseButtons.Left && CTRL)
+                Application.Exit();
+            if(e.Button==MouseButtons.Right)
+            {
+                Text = rect.Size.ToString();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control)
+                CTRL = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Control)
+                CTRL = false;
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Text = "("+e.X.ToString() + " : "+e.Y.ToString()+")";
+        }
+    }
+}
