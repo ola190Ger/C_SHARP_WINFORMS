5eb97575c31d4fdf8af56e393d4a0eb88294121e
ola190Ger <49449139+ola190Ger@users.noreply.github.com>
2019-11-06 20:56:07 +0300

    Finish

---
+++ dz3_4_текстовый файл/dz3_4_текстовый файл/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace dz3_4_текстовый_файл
+{
+    public delegate void ReturnText(string[] str);
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if(openFile.ShowDialog()==DialogResult.OK)
+            {
+                string filename = openFile.FileName;
+                textBox1.Lines=(File.ReadAllLines(filename));
+                button2.Enabled = true;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2(textBox1.Lines, Returntxt);
+            f2.Show();
+        }
+
+        public void Returntxt(string[] str)
+        {
+            textBox1.Lines = str;
+        }
+    }
+}
---
+++ dz3_4_текстовый файл/dz3_4_текстовый файл/Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dz3_4_текстовый_файл
+{
+    public partial class Form2 : Form
+    {
+        ReturnText Return;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Form2(string[] str,ReturnText rt)
+        {
+            InitializeComponent();
+            textBox1.Lines = str;
+            //foreach(string txt in str)
+                //textBox1.Lines.Append(txt);
+            this.Return = rt;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Return(textBox1.Lines);
+            DialogResult res= MessageBox.Show("Продолжить редактирование?", "Что делаем...", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res == DialogResult.No)
+                Close();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
