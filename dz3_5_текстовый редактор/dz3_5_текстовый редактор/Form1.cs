e61e699205700498cdac053320f598e8b4989245
ola190Ger <49449139+ola190Ger@users.noreply.github.com>
2019-11-12 20:05:17 +0300

    upload repos Step

--- dz3_5_текстовый редактор/dz3_5_текстовый редактор/Form1.cs
+++ dz3_5_текстовый редактор/dz3_5_текстовый редактор/Form1.cs
@@ -16,6 +16,7 @@ namespace dz3_5_текстовый_редактор
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void ToolStripButton2_Click(object sender, EventArgs e)
@@ -26,10 +27,47 @@ namespace dz3_5_текстовый_редактор
         private void OpenTxt()
         {
             OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text fales(*.txt)|*.txt|All files(*.*)|*.*";
             if(ofd.ShowDialog()==DialogResult.OK)
             {
                 textBox1.Lines = File.ReadAllLines(ofd.FileName);
             }
+        }
+
+        private void ОткрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenTxt();
+        }
+
+        private void ToolStripButton3_Click(object sender, EventArgs e)
+        {
+            SaveTxt();
+        }
+        private void SaveTxt()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text fales(*.txt)|*.txt|All files(*.*)|*.*";
+            if (sfd.ShowDialog()==DialogResult.OK)
+            {
+                File.WriteAllText(sfd.FileName, textBox1.Text);
+                MessageBox.Show("Файл сохранен");
+            }
+        }
+
+        private void СохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveTxt();
+        }
+
+        private void СохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveTxt();
+        }
+
+        private void НовыйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //что делать если какойто текст уже есть
+            textBox1.Text = "";
         }
     }
 }
