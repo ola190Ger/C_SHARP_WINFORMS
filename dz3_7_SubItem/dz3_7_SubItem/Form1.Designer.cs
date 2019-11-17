5f00b5abb3e42473f9b6e74bcfa7bb1181200081
ola190Ger <49449139+ola190Ger@users.noreply.github.com>
2019-11-17 22:59:12 +0200

    Начало

---
+++ dz3_7_SubItem/dz3_7_SubItem/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace dz3_7_SubItem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.textBox1.Location = new System.Drawing.Point(41, 257);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "TopLevelMenu";
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TextBox1_MouseDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.textBox2.Location = new System.Drawing.Point(242, 257);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "SubItem";
+            this.textBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TextBox2_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить пункт меню";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(264, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Добавить подменю";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(466, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ dz3_7_SubItem/dz3_7_SubItem/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dz3_7_SubItem
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void TextBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.ForeColor = Color.Black;
+        }
+
+        private void TextBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox2.Text = "";
+            textBox2.ForeColor = Color.Black;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                MessageBox.Show("Введите название пункта меню.", "Отсутствует имя");
+            else
+            {
+                //ToolStripMenuItem fileItem = new ToolStripMenuItem(textBox1.Text);
+
+                //fileItem.DropDownItems.Add("Создать");
+                //fileItem.DropDownItems.Add(new ToolStripMenuItem("Сохранить"));
+
+                menuStrip1.Items.Add(new ToolStripMenuItem(textBox1.Text));
+
+
+                ToolStripMenuItem aboutItem = new ToolStripMenuItem("О программе");
+                menuStrip1.Items.Add(aboutItem);
+
+                MessageBox.Show($"{menuStrip1.Items[1]}", "About program");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if(textBox2.Text=="")
+                MessageBox.Show("Введите название подпункта меню.", "Отсутствует имя");
+            else
+            {
+                //ToolStripMenuItem submenu = new ToolStripMenuItem(textBox2.Text);
+
+                if (menuStrip1.Items.Count == 0)
+                    MessageBox.Show(" Подменю не может быть добавлено. Введите имя меню и подменю");
+                else
+                    if (menuStrip1.Items.Count == 1)
+                    {
+                    (menuStrip1.Items[0] as ToolStripMenuItem).DropDownItems.Add(textBox2.Text);
+                    }
+                    
+
+                else
+                {
+                    //(menuStrip1.Items[0] as ToolStripMenuItem).DropDownItems.Add(textBox2.Text);
+
+                    //укажи имя меню для подменю
+                }
+
+            }
+        }
+    }
+}
